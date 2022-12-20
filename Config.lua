e649b0292ad1a458878fc135d314cc6e6786e368
aklevraie <121125917+aklevraie@users.noreply.github.com>
2022-12-20 19:36:32 +0100

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,27 @@
+
+
+Config = {}
+
+Config.Items = {
+
+-- 'item' 	est le nom de l'item dans la bdd ( pas le label )
+-- 'prix' 	est le prix de vente avec une marge 10% 
+-- 'legal' 	si mis sur false la police sera prevenu 1/Chance du temps 
+-- 'sale'	si l'argent est donné en sale
+
+--	{ item = '', 		prix = , 	legal = ,	sale = },
+
+-- FOR YOU EVIE
+
+	 	
+	{ item = 'weed_pooch', 	prix = 50, 	legal = false,	sale =  false },	
+	{ item = 'coke_pooch', 	prix = 70, 	legal = false,	sale =  false },	
+	{ item = 'meth_pooch', 	prix = 80, 	legal = false,	sale =  false },	
+	{ item = 'opium_pooch', prix = 100, 	legal = false,	sale =  false },
+
+	
+	{ item = 'cigare', 	prix = 45, 	legal = true,	sale =  false },	
+	{ item = 'gitanes', 	prix = 20, 	legal = true,	sale =  false },
+	
+
+}
